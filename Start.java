838e7e6f6116b40d1fa6890782fb9819758920b6
Shadman-Shakib-Nihan <153739094+Shadman-Shakib-Nihan@users.noreply.github.com>
2024-12-25 19:55:03 +0600

    Main

---
+++ Start.java
@@ -0,0 +1,13 @@
+import java.lang.*;
+import classes.*;
+import javax.swing.*;
+import java.awt.*;
+import entity.*;
+
+
+public class Start {
+    public static void main(String[] args) {
+       new Welcome();
+        
+    }
+}
